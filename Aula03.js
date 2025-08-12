947ccd5675c139089521b4f4cdc53d36ad495cf8
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-08-12 20:54:15 -0300

    feat: Inclusão da questão 03 e de sua resolução

--- Aula03.js
+++ Aula03.js
@@ -1,5 +1,4 @@
 // 01) Receber cinco notas e calcular a média entre elas:
-
 // Declaração de variáveis
 primeiraNota = 0
 segundaNota = 0
@@ -48,3 +47,20 @@ temperaturaCelsius = (5/9) * (temperaturaFahrenheit - 32)
 
 // Mostrar o resultado para o usuário no console
 console.log("A temperatura em Celsius é: " + temperaturaCelsius + "°C")
+
+
+// 03) Elabore um script que calcule a hipotenusa de um triângulo retângulo (algoritmo de Pitágoras)
+// Declaração de variáveis
+cateto01 = 0 // lado 01
+cateto02 = 0 // lado 02
+hipotenusa = 0
+
+// Receber informações do usuário
+cateto01 = prompt("Insira o primeiro lado (cateto) do triâgulo retângulo:")
+cateto02 = prompt("Insira o segundo lado (cateto) do triâgulo retângulo:")
+
+// Calcular o que se pede
+hipotenusa = ((cateto01 ** 2) + (cateto02 ** 2)) ** 0.5
+
+// Mostrar o resultado para o usuário no console
+console.log("A hipotenusa é: " + hipotenusa)
