4798324d7d0716368b31126d5b81a01a73504713
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-08-19 19:53:24 -0300

    feat: alteração do nome do arquivo para Aula05.js, inserção do resultado do exercício 01

---
+++ Modulo_01/Aula05.js
@@ -0,0 +1,27 @@
+// OBS: Aula 04 foi teórica, não teve exercícios.
+
+// 01) Elabora um script que calcule o IMC (Índice de Massa Corporal) de uma pessoa:
+// Declaração de variáveis
+peso = 0 // lado 01
+altura = 0 // lado 02
+imc = 0 // Índice de Massa Corporal
+
+// Receber informações do usuário
+peso = prompt("Insira o peso da pessoa (kg):").replace(',', '.')
+altura = prompt("Insira a altura da pessoa (metro):").replace(',', '.')
+
+//Formatação
+peso = Number(peso)
+altura = Number(altura)
+
+pesoFormatado = peso.toFixed(2)
+alturaFormatada = altura.toFixed(2)
+
+// Calcular o que se pede
+imc = pesoFormatado /(alturaFormatada ** 2)
+
+//Formatação
+imcFormatado = imc.toFixed(2).replace('.', ',')
+
+// Mostrar o resultado para o usuário no console
+console.log("O IMC da pessoa é: " + imcFormatado)
