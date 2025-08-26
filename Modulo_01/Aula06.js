8d65c19f480558f5ec2b31d6376606aa008cb90d
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-08-26 11:03:14 -0300

    feat: Inclusão do exercício 01 e de sua resolução

--- Modulo_01/Aula06.js
+++ Modulo_01/Aula06.js
@@ -1,1 +1,17 @@
+// 01) Faça um programa que receba a idade e retorne se a pessoa pode tirar CNH.
+// Declaração de Variáveis
+idadeUsuario = 0
+limiteIdadeMinima = 18
+saldoIdade = 0
+
+//Recebe informações do Usuário
+idadeUsuario = Number(idadeUsuario)
+
+//Desvio Condicional
+if(idadeUsuario >= limiteIdadeMinima){
+  console.log("Parabéns! Você já pode tirar sua habilitação de Motorista.")
+} else {
+  saldoIdade = limiteIdadeMinima - idadeUsuario
+  console.log("Falta" + saldoIdade + " anos para você tirar a sua Habilitação de Morotista")
+}
 
