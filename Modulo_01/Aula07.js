2d9c5878dc44896c1d665b276aec7eb45e1a056d
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-08-26 20:48:05 -0300

    feat:  Inclusão da Questão 09 da Aula07.js

--- Modulo_01/Aula07.js
+++ Modulo_01/Aula07.js
@@ -1,1 +1,17 @@
+// 09) Faça um script que mostrar a classificação de um filme conforme a faixa etária indicada pelo usuário:
 
+opcaoUsuario = prompt("Digite a idade a ser consultada:")
+
+opcaoUsuario = parseInt(opcaoUsuario)
+
+if (opcaoUsuario < 12){
+    console.log("Faixa etária: Menor que 12 anos.\n Classificação: Livre")
+} else if (opcaoUsuario >= 12 && opcaoUsuario <= 14){
+    console.log("Faixa etária: De 12 a 14 anos.\n Classificação: +12")
+} else if (opcaoUsuario >= 15 && opcaoUsuario <= 17){
+    console.log("Faixa etária: De 15 a 17 anos.\n Classificação: +15")
+} else if (opcaoUsuario >= 18){
+    console.log("Faixa etária: 18 anos ou mais.\n Classificação: +18")
+} else {
+  console.log("Por gentileza, escolha uma idade")
+}
