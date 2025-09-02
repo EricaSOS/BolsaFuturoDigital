4b674f020cc55b0557f9ffe2d1b80a9dd6b32304
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-09-02 19:51:03 -0300

    feat: Update Aula08.js with examples (While) and challenge 01

--- Modulo_01/Aula08.js
+++ Modulo_01/Aula08.js
@@ -1,1 +1,35 @@
+// EXEMPLOS DE ESTRUTURAS DE REPETIÇÃO
+// 1) WHILE
+contador = 1
+while (contador <= 5){
+  console.log("Número:" + contador)
+  contador++
+}
 
+//Com break
+contador = 1
+while (true){
+  console.log("Número:" + contador)
+  contador++
+
+  if(contador > 5){
+    break
+  }
+}
+
+//Com continue
+
+
+// DESAFIO 01: JOGO DO PIM (SILVIO SANTOS)
+contador = 1
+
+while (contador <= 25){ 
+
+  if(contador % 3 !== 0){
+    console.log(contador)
+  } else {
+    console.log("PIM")
+  }
+  
+  contador++
+}
