250275c00aebeca60d75a1da429b14714d480d20
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-09-02 20:53:11 -0300

    feat: Update Aula08.js, inclusão de exemplo de do/while, for e desafio 02

--- Modulo_01/Aula08.js
+++ Modulo_01/Aula08.js
@@ -17,8 +17,20 @@ while (true){
   }
 }
 
-//Com continue
+// 2) DO / WHILE
 
+contador = 1
+
+do{
+  console.log("Número:" + contador)
+  contador++
+} while (contador <= 5)
+
+// 3) FOR
+
+for(contador = 1; contador <= 5; contador ++){
+  console.log("Número: " + contador)
+}
 
 // DESAFIO 01: JOGO DO PIM (SILVIO SANTOS)
 contador = 1
@@ -33,3 +45,11 @@ while (contador <= 25){
   
   contador++
 }
+
+// DESAFIO 02: Imprima os números ímpares de 0 a 25
+
+for(contador = 1; contador <= 25; contador+= 2){
+  console.log("Número: " + contador)
+}
+
+
