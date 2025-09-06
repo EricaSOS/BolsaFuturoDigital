e6a1f3c23160ffd46ce1f0e95986d7f9e777e515
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-09-06 09:18:06 -0300

    feat: inclusão da atividade 14

--- Modulo_01/Aula09.js
+++ Modulo_01/Aula09.js
@@ -1,1 +1,24 @@
+/* 14) Desenvolver uma solução para mostrar qual o valor final de um investimento financeiro.
+Receber um valor como capital de entrada, número de meses aplicados e taxa de rentabilidade ao mês.
+Ao final imprimir: Capital de entrada, meses, taxa e valor capitalizado (final)
+*/
 
+capitalEntrada = prompt("Informe o valor inicial do investimento:")
+meses = prompt("Informe por quanto tempo (meses) deseja investir:")
+taxa = prompt("Informe a taxa do investimento:")
+
+capitalFinal = parseFloat(capitalEntrada)
+taxa = parseFloat(taxa)
+
+for(contador = 1; contador <= meses; contador++){
+  capitalFinal = capitalFinal + (capitalFinal * taxa)
+  console.log(capitalFinal)
+}
+
+capitalEntrada = parseFloat(capitalEntrada).toFixed(2)
+capitalFinal = parseFloat(capitalFinal).toFixed(2)
+
+console.log("Valor inicial: R$ " + capitalEntrada)
+console.log("Perído da aplicação: " + meses + " meses")
+console.log("Taxa: " + taxa)
+console.log("Valor ao final da aplicação: R$ " + capitalFinal)
