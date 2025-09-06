8643bfcf556a3a0ad09e63e1694f12edf7d2165d
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-09-06 09:22:22 -0300

    feat: tratamento da vírgula e do ponto, para adequar ao sistema decimal brasileiro

--- Modulo_01/Aula09.js
+++ Modulo_01/Aula09.js
@@ -2,10 +2,9 @@
 Receber um valor como capital de entrada, número de meses aplicados e taxa de rentabilidade ao mês.
 Ao final imprimir: Capital de entrada, meses, taxa e valor capitalizado (final)
 */
-
-capitalEntrada = prompt("Informe o valor inicial do investimento:")
+capitalEntrada = prompt("Informe o valor inicial do investimento:").replace(",",".") // tratamento da vírgula da entrada do usuário brasileiro
 meses = prompt("Informe por quanto tempo (meses) deseja investir:")
-taxa = prompt("Informe a taxa do investimento:")
+taxa = prompt("Informe a taxa do investimento (em decimais):").replace(",",".") // tratamento da vírgula da entrada do usuário brasileiro
 
 capitalFinal = parseFloat(capitalEntrada)
 taxa = parseFloat(taxa)
@@ -15,8 +14,8 @@ for(contador = 1; contador <= meses; contador++){
   console.log(capitalFinal)
 }
 
-capitalEntrada = parseFloat(capitalEntrada).toFixed(2)
-capitalFinal = parseFloat(capitalFinal).toFixed(2)
+capitalEntrada = parseFloat(capitalEntrada).toFixed(2).replace(".",",") // tratamento do ponto para aparecer vírgula para entendimento de usuário brasileiro
+capitalFinal = parseFloat(capitalFinal).toFixed(2).replace(".",",") // tratamento do ponto para aparecer vírgula para entendimento de usuário brasileiro
 
 console.log("Valor inicial: R$ " + capitalEntrada)
 console.log("Perído da aplicação: " + meses + " meses")
