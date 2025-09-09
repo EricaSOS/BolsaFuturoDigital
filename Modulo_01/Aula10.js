2132e7cf304a7c07617af41441b063994a138473
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-09-09 20:25:35 -0300

    feat: inclusão de exemplos de laço de repetição e splice Aula10.js

--- Modulo_01/Aula10.js
+++ Modulo_01/Aula10.js
@@ -39,3 +39,25 @@ tamanho = fibonacci.push(21)
 
 console.log(tamanho)
 console.log(fibonacci)
+
+// excluindo elementos com splice
+fibonacci = [1, 2, 3, 5, 8, 13]
+
+console.log(fibonacci.length)
+
+fibonacci.splice(1, 2) //remove 02 elementos a partir do índice 1
+console.log(fibonacci.length)
+console.log(fibonacci)
+
+
+fibonacci.splice(1, 0, "três", "quatro") //remove 0 elementos a partir do índice 1 e insere os demais
+console.log(fibonacci.length)
+console.log(fibonacci)
+
+// como usar num laço de repetição:
+fibonacci = [1, 2, 3, 5, 8, 13]
+
+for(indice=0; indice < fibonacci.length; indice++){
+  console.log (fibonacci[indice])
+}
+
