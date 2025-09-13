3cb4675799f7040ea3579dd42afaa2a3888dd868
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-09-13 09:01:11 -0300

    feat: atualização da resposta do Desafio01.js

--- Modulo_01/Desafio01.js
+++ Modulo_01/Desafio01.js
@@ -3,37 +3,23 @@
 - Ao final, o sistema deve mostrar o cupom da venda.
 */
 
-descricaoProduto = new Array()
-quantidadeProduto = new Array()
-valorUnidadeProduto = new Array()
-valorTotalItem = new Array()
+descricaoProduto = new Array() //ITEM       DESCRIÇÃO            QTD   VALOR      TOTAL
 
 quantidadeItens = 0
 valorItem = 0
 valorTotalItem = 0
-calculoItem = 0
-
 valorTotalCompra = 0
-
 
 console.log("Olá, vamos iniciar o registro dos seus produtos.\n Para finalizar, digite 'SSSS' no campo produto.)")
 nomeProduto = prompt("Informe o produto:")
 
-for(itemCompra = 1, nomeProduto != "SSSS", itemCompra++){
-  descricaoProduto.push(nomeProduto)
+for(itemCompra = 1; nomeProduto != "SSSS"; itemCompra++){
   quantidadeItens = Number(prompt("Quantidade de produtos: "))
-  quantidadeProduto.push(quantidadeItens)
   valorItem = parseFloat(prompt("Valor do produto (R$)")).replace(",",".")
-  valorUnidadeProduto.push(valorItem)
-  calculoItem = quantidadeItens * valorItem
-  valorTotalItem.push(calculoItem)
-
-  valorProduto = parseFloat(valorProduto)
-  totalProduto = totalProduto + valorProduto
-  contadorProduto++
-  valorProduto = valorProduto.toFixed(2).replace(".",",")
-  console.log("Produto " + contadorProduto + ": " + nomeProduto +" ........... R$ " + valorProduto)
-  nomeProduto = prompt("Informe o produto:")
+  valorTotalItem = quantidadeItens * valorItem
+  valorTotalCompra = valorTotalCompra + valorTotalItem  
+  descricaoProduto.push(itemCompra, nomeProduto, quantidadeItens, valorItem, valorTotalItem)
+  nomeProduto = prompt("Informe o produto:")  
 }
   
   
