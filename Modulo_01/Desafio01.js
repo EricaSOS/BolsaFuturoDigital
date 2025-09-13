245c5927a10a1dfa4c30b9e6d8ffd5337d55c99f
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-09-13 08:13:24 -0300

    feat: inclusão do comando do Desafio01.js

--- Modulo_01/Desafio01.js
+++ Modulo_01/Desafio01.js
@@ -1,1 +1,4 @@
-
+/* Desafio: Melhorar o simulador de um caixa de supermercado (exercício de fixação 15).
+- O usuário precisa informar: a descrição e o valor do produto e a quantidade comprada
+- Ao final, o sistema deve mostrar o cupom da venda.
+*/
