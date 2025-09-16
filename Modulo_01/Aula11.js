82a67240c63cd36ee9c89e94c25c414d0d331b5c
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-09-16 19:32:40 -0300

    feat: parte teórica Aula11.js

--- Modulo_01/Aula11.js
+++ Modulo_01/Aula11.js
@@ -1,1 +1,23 @@
+// função que soma dois números
+function somar(a,b){
+  return a + b
+}
+// resultado da soma de dois números
+resultado = somar(5,7)
+console.log("O resultado é: " + resultado)
 
+// Arrow Function (Função abreviada)
+somar = (a,b) => {
+  return a + b
+}
+
+somar2 = (a,b) => a + b
+
+dobro = x => x * 2
+
+resultado = somar(5,7)
+console.log("O resultado da soma é: " + resultado)
+resultado = somar2(10,7)
+console.log("O resultado da soma2 é: " + resultado)
+resultado = dobro(6)
+console.log("O resultado do dobro é: " + resultado)
