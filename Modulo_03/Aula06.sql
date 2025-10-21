934bf93edab4813cd356ecb53b9212aee5cf59f9
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-10-21 19:44:36 -0300

    feat: inclusão de exemplo e exercício 09 sobre atualização de dados  Aula06.sql

--- Modulo_03/Aula06.sql
+++ Modulo_03/Aula06.sql
@@ -1,1 +1,9 @@
-
+-- EXEMPLO ATUALIZAR REGISTROS
+UPDATE PRODUTO
+    SET VAL_UNIT = 8.00, UNID_PROD = 'KG'
+    WHERE DESC_PROD = 'CIMENTO' OR DESC_PROD = 'FIO PLASTICO';
+    
+-- EXERCÍCIO 09: Atualizar o salário fixo de todos os vendedores em 27% mais uma bonificação de R$ 100,00
+UPDATE VENDEDOR
+    SET SAL_FIXO = ((SAL_FIXO * 1.27) + 100)
+    
