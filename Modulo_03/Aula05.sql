ed9f74325dd19902a041cf4dad1cc51afbdd1e9b
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-10-26 10:38:17 -0300

    feat: Rename Aula06.sql to Aula05.sql, exclusão das respostass do desafio, para incluir num arquivo específico

---
+++ Modulo_03/Aula05.sql
@@ -0,0 +1,12 @@
+-- EXEMPLO ATUALIZAR REGISTROS
+UPDATE PRODUTO
+    SET VAL_UNIT = 8.00, UNID_PROD = 'KG'
+    WHERE DESC_PROD = 'CIMENTO' OR DESC_PROD = 'FIO PLASTICO';
+    
+-- EXERCÍCIO 09: Atualizar o salário fixo de todos os vendedores em 27% mais uma bonificação de R$ 100,00
+UPDATE VENDEDOR
+    SET SAL_FIXO = ((SAL_FIXO * 1.27) + 100)
+    
+-- EXEMPLO DE EXCLUSÃO DE DADOS
+DELETE FROM <TABELA>
+    WHERE <CONDIÇÃO>;
