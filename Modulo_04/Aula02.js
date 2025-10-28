bc664338b9a5728b24d4d66bd56b0e23eb324db7
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-10-28 19:57:17 -0300

    feat: inclusão de rota e servidor Update Aula02.js

--- Modulo_04/Aula02.js
+++ Modulo_04/Aula02.js
@@ -1,1 +1,22 @@
+//server.js
+import {createServer} from 'node:http';
 
+const server = createServer((req,res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+
+    if (req.url === '/') {
+        res.write('<h1>P&aacute;gina principal</h1>')
+    } else if (req.url ==='/hello') {
+        res.write('<h1> Ol&aacute;, mundo!<h1>')
+    } else {
+        res.write('<h1>P&aacute;gina n&atilde;o encontrada!</h1>')
+    }
+    res.end();
+    });
+
+    server.listen(3000, '127.0.0.1', () => {
+        console.log('Listening on 127.0.0.1:3000');
+    });
+
+    //run with 'node server.js'
