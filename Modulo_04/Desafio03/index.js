3ec773ebdc0b26f4f91b14864f561ae19c7b68f9
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-11-25 21:49:18 -0300

    Update index.js

--- Modulo_04/Desafio03/index.js
+++ Modulo_04/Desafio03/index.js
@@ -25,7 +25,7 @@ app.get('/pedidos', (req, res) => {
     const htmlBase = fs.readFileSync('./views/views.html') //Lê o html
     const cheerioLoad = cheerio.load(htmlBase) // carrega no cheerio o html
     const tabelaCorpo = cheerioLoad('#tabela-corpo') //pega apenas a parte da tabela-corpo
-    const tabelaRodape = cheerioLoad('#tabela-rodape') //pega apenas a parte da tabela-rodape
+    //const tabelaRodape = cheerioLoad('#tabela-rodape') //pega apenas a parte da tabela-rodape
 
         rows.forEach(pedido => {
             tabelaCorpo.append(`                
@@ -43,22 +43,45 @@ app.get('/pedidos', (req, res) => {
 })
 
 app.get('/pedido/:id', (req, res, next) => {
-    res.send('ID: ' + req.params.id)
     //Buscar esse pedido no banco
-    const sql = `SELECT * FROM ITEM_PEDIDO 
-                WHERE NO_PED = ?`
+    const sql = `SELECT PROD.COD_PROD, PROD.DESC_PROD, PROD.UNID_PROD, IPED.QTD_PED, PROD.VAL_UNIT, (IPED.QTD_PED * PROD.VAL_UNIT) AS 'VAL_TOTAL', SUM(IPED.QTD_PED * PROD.VAL_UNIT) AS VALOR_TOTAL
+                    FROM ITEM_PEDIDO IPED, PEDIDO PED, PRODUTO PROD
+                        WHERE IPED.NO_PED = PED.NUM_PED
+                            AND IPED.CD_PROD = PROD.COD_PROD
+                            AND PED.NUM_PED = ?`
     
     db.all(sql, [req.params.id], (err,rows) => {
         if (err) {
             res.status(500).json({error: err.message})
             return
-        }
-    })
+        }    
     
     //Carregar o Html
+    const htmlBase = fs.readFileSync('./views/pedido.html') //Lê o html
+    const cheerioLoad = cheerio.load(htmlBase) // carrega no cheerio o html
+    const itensPedido = cheerioLoad('#items-pedido') //pega apenas a parte da tabela-corpo
+    const totalPedido = cheerioLoad('#total-pedido')
     // Preencher os valores
+
+    
+    rows.forEach(itemPedido => {
+        itensPedido.append(`
+                <div class="row g-0">
+                    <div class="col-1 text-center">${itemPedido.COD_PROD}</div>
+                    <div class="col-4">${itemPedido.DESC_PROD}</div>
+                    <div class="col-1 text-center">${itemPedido.UNID_PROD}</div>
+                    <div class="col-2 text-center">${itemPedido.QTD_PED}</div>
+                    <div class="col-2 text-right">R$ ${itemPedido.VAL_UNIT}</div>
+                    <div class="col-2 text-right">R$ ${itemPedido.VAL_TOTAL}</div>
+                </div>
+                `)
+        })
+    
     //Enviar o HTML
+    res.send(cheerioLoad.html())
+    })
 })
+
 
 app.listen(port, () => {
     console.log(`Servidor ouvindo a porta ${port}!`)
